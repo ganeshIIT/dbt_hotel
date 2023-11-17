8edbd72594f5444da3b927ac5782b32328dc48b4
Ganesh <gannyprodigy@gmail.com>
2023-11-17 06:07:07 +0000

    fixed

--- snapshots/snapshot_customers.sql
+++ snapshots/snapshot_customers.sql
@@ -4,6 +4,7 @@
       unique_key= 'customer_hkey',
       strategy='check',
       check_cols=['customer_hdiff'],
+      updated_at='updated'
     )
 }}
 select * from {{ ref('stg_raw_customers') }}
