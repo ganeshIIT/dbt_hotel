f7cf3b624c1b618a4a7246f340cdb53a5284547c
ganesh <ganeshlcontact@gmail.com>
2023-11-20 16:02:22 +0530

    implemented macro based incremental load for reservations

--- _queries/queries.sql
+++ _queries/queries.sql
@@ -14,3 +14,8 @@ clone hotel_prod.raw.reservations;
 select object_insert(reservation_data, 'STATUS', 'cancelled', true) from hotel_dbt.test.test_reservations;
 
 select * from hotel_dbt.gl.hist_reservations;
+select max(dbt_batch_utc) from hotel_dbt.gl.hist_reservations;
+select max(from_date) from hotel_dbt.gl.hist_customers;
+
+select * from hotel_dbt.snapshots.snapshot_customers;
+
--- models/history/hist_reservations.sql
+++ models/history/hist_reservations.sql
@@ -1,4 +1,14 @@
-{{
+{{  save_history(
+        input_rel = ref('stg_raw_reservations'),
+        key_column = 'reservation_hkey',
+        diff_column = 'reservation_hdiff',
+        load_ts_column = 'updated',
+
+        high_watermark_column = 'updated'
+) }}
+
+
+{# {{
     config(
         materialized='incremental',
         on_schema_change='fail'
@@ -17,4 +27,4 @@ select * from reservations
 
   where updated > (select max(updated) from {{ this }})
 
-{% endif %}
+{% endif %} #}
